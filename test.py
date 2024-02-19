533d0a6cd31098abfcc51c34a5dcb2e151da3aec
mkillah <marko.antolcic@gmail.com>
2024-02-19 00:08:36 +0000

    added lc0 - to use maia weights, just put them in lc0 file

---
+++ test.py
@@ -0,0 +1,13 @@
+import chess
+import chess.engine
+
+engine = chess.engine.SimpleEngine.popen_uci(r"C:/Users/marko/PycharmProjects/maia-chess-virtualchessplayer/lc0/lc0.exe")
+
+board = chess.Board("r1bqkbnr/p1pp1ppp/1pn5/4p3/2B1P3/5Q2/PPPP1PPP/RNB1K1NR w KQkq - 2 4")
+info = engine.analyse(board, chess.engine.Limit(depth=1))
+result = engine.play(board, chess.engine.Limit(time=0.1))
+print("Score:", info["score"])
+# Score: PovScore(Mate(+1), WHITE)
+print("Best move:", result.move)
+
+engine.quit()
